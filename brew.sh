51bee18371437266960fda4d1f289316e20791b4
aveg <35672644+averygue@users.noreply.github.com>
2018-03-02 11:36:37 -0600

    brew.sh

---
+++ brew.sh
@@ -0,0 +1,14 @@
+#install homebrew via link at https://brew.sh/
+
+
+brew install git
+brew install curl
+brew install vim
+brew install textmate
+#or brew install atom
+
+brew install slack
+brew install virtualbox
+brew install spotify
+
+brew cask install 1password
